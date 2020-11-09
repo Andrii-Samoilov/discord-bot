b47dec30ad4699b981c2fd27c54dd17c963bbad0
Andrew Dark <44619345+Andrew-Dark@users.noreply.github.com>
2020-11-09 19:20:32 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,44 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+bot.commands = new Discord.Collection();
+const db = require("quick.db");
+const fs = require("fs");
+let config = require("./botconfig.json");
+let token = config.token;
+let prefix = config.prefix;
+fs.readdir("./cmds/", (err, files) => {
+  if (err) console.log(err);
+  let jsfiles = files.filter(f => f.split(".").pop() === "js");
+  if (jsfiles.length <= 0) console.log("Нет комманд для загрузки!!");
+  console.log(`Загружено ${jsfiles.length} комманд`);
+  jsfiles.forEach((f, i) => {
+    let props = require(`./cmds/${f}`);
+    console.log(`${i + 1}.${f} Загружен!`);
+    bot.commands.set(props.help.name, props);
+  });
+});
+bot.on('ready', () =>{ // ивент, когда бот запускается https://discord.js.org/#/docs/main/stable/class/Client?scrollTo=e-ready
+  bot.generateInvite("ADMINISTRATOR").then(invite => console.log(`Ссылка на добавление ${invite}`))
+  bot.user.setPresence({
+    status: 'online',
+    activity: {
+    name: `Working`
+  }
+  })
+})
+
+bot.on('message', message =>{ // ивент, когда приходит любое сообщение в чат https://discord.js.org/#/docs/main/stable/class/Client?scrollTo=e-message
+    if (message.author.bot) return;
+    if (message.channel.type === 'dm') {
+return //Игнорировать сообщения в личку 
+}
+    let messageArray = message.content.split(" ");
+    let command = messageArray[0].toLowerCase();
+    let args = messageArray.slice(1);
+    if(!message.content.startsWith(prefix)) return;
+   let cmd = bot.commands.get(command.slice(prefix.length));
+    if (cmd){
+      cmd.run(bot, message, args);
+    }
+  });
+bot.login(token)
