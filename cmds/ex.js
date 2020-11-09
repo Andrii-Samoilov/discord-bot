bccffedb4ca5a58d5da049160de02b5ceace5b8c
Andrew Dark <44619345+Andrew-Dark@users.noreply.github.com>
2020-11-09 19:31:00 +0200

    Create ex.js

---
+++ cmds/ex.js
@@ -0,0 +1,8 @@
+const Discord = module.require("discord.js");
+module.exports.run = async (bot, message, args) => {
+ //Тут пишите код команды
+};
+
+module.exports.help = {
+  name: "name" //название команды по которому она будет вызываться
+};
